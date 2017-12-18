832bac848d32123d42a8165bb575340ed41e6f55
Patrik Svensson <patrik@patriksvensson.se>
2017-12-18 18:16:21 +0100

    Initial commit.

---
+++ src/Cake/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cake
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        { 
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
